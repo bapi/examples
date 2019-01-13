94a2328a52bb4f5d3779c0507dcc3ca730d86633
U-IST\bchatter <bchatter@wngrad096.ist.local>
2019-01-13 11:53:09 +0100

    update

--- mnist1/main.py
+++ mnist1/main.py
@@ -99,7 +99,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
@@ -152,7 +152,11 @@ def main():
     results = torch.zeros(args.epochs,3)
     results.share_memory_()
     
-    f = open('stochastic_gradient_descent'+'_batch_size='+str(args.batch_size)+'_usebackprop='+str(args.usemysgd)+'.txt',"w")
+    if args.usemysgd:
+      f = open('stochastic_gradient_descent'+'_batch_size='+str(args.batch_size)+'_usebackprop=True.txt',"w")
+    else:
+      f = open('stochastic_gradient_descent'+'_batch_size='+str(args.batch_size)+'_usebackprop=False.txt',"w")
+
     print('Stochastic Gradient descent: Batch-size = {}'.format(args.batch_size))
     f.write('Stochastic Gradient descent: Batch-size = {}'.format(args.batch_size))
     f.write("\n\nEpoch\tLR\tLoss\tAccuracy\n\n")
