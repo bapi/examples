fa0338aaedd06c05d707b670bafa1df57a58b312
U-IST\bchatter <bchatter@wngrad096.ist.local>
2019-01-13 12:35:39 +0100

    updates

---
+++ mnist_hogwild2/main.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+import argparse
+import time 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.multiprocessing as mp
+from torch.multiprocessing import Process, Value, Lock, Queue
+
+from mysgd import StochasticGD
+from train import train, test
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--num-processes', type=int, default=2, metavar='N',
+                    help='how many training processes to use (default: 2)')
+parser.add_argument('--usemysgd', type=int, default=1, metavar='U',
+                        help='Whether to use custom SGD')
+parser.add_argument('--lra', type=bool, default=True, metavar='LR',
+                        help='Whether to use adaptable learning rate')
+    
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
+
+    model = Net()
+    model.share_memory() # gradients are allocated lazily, so they are not shared here
+    
+    val = Value('i', 0)
+    lock = Lock()
+    results = torch.zeros(args.epochs,3)
+    results.share_memory_()
+    
+    if args.usemysgd:
+      f = open('hogwild'+'_batch_size='+str(args.batch_size)+'_numproc='+str(args.num_processes)+'_usebackprop=True.txt',"w")
+    else:
+      f = open('hoigwild'+'_batch_size='+str(args.batch_size)+'_numproc='+str(args.num_processes)+'_usebackprop=False.txt',"w")
+
+    # f = open('stochastic_gradient_descent'+'_batch_size='+str(args.batch_size)+'usebackprop='+str(args.usemysgd)+'.txt',"w")
+    print('Stochastic Gradient descent: Batch-size = {}'.format(args.batch_size))
+    f.write('Stochastic Gradient descent: Batch-size = {}'.format(args.batch_size))
+    f.write("\n\nEpoch\tLR\tLoss\tAccuracy\n\n")
+    start = time.time()
+    
+    processes = []
+    for rank in range(args.num_processes):
+        p = mp.Process(target=train, args=(rank, args, model, results))
+        # We first train the model across `num_processes` processes
+        p.start()
+        processes.append(p)
+    p = mp.Process(target=test, args=(args, model, results, val, lock))
+    p.start()
+    processes.append(p)
+    for p in processes:
+        p.join()
+
+    train_end = time.time()
+    train_time = (train_end - start)
+    
+    for i in range(args.epochs):
+      f.write('{}\t'.format(i))
+      f.write(str('%.6f'%results[i][0].item())+"\t")
+      f.write(str('%.6f'%results[i][1].item())+"\t")
+      f.write(str('%.6f'%results[i][2].item())+"\n")
+      
+
+    print("Training time = " + str(train_time)) 
+    f.write("\n\nTraining time = " + str(train_time)) 
+    f.close()
---
+++ mnist_hogwild2/train.py
@@ -0,0 +1,97 @@
+import os
+import torch
+import torch.optim as optim
+import torch.optim.lr_scheduler as lrs
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+from mysgd import StochasticGD
+
+def train(rank, args, model, result):
+    torch.manual_seed(args.seed + rank)
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                    ])),
+        batch_size=args.batch_size, shuffle=True, num_workers=1)
+
+    if args.usemysgd:
+      optimizer = StochasticGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    else:
+      optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    scheduler = lrs.ExponentialLR(optimizer, gamma = 0.95)
+    for epoch in range(1, args.epochs + 1):
+        if args.lra:
+          scheduler.step()
+        train_epoch(epoch, args, model, train_loader, optimizer)
+        if rank == 0:
+          result[epoch-1][0] = get_lr(optimizer)
+
+def test(args, model, results, val, lock):
+    torch.manual_seed(args.seed)
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=args.batch_size, shuffle=True, num_workers=1)
+
+    # l,a = test_epoch(model, test_loader)
+    counter = 0
+    np = args.num_processes
+    while counter < args.epochs:
+      if val.value > 0 and val.value % np == 0:
+        with lock:
+          val.value -= np
+        l,a = test_epoch(model, test_loader)
+        print("Epoch: "+ str(counter) + " Test_loss= " + str('%.6f'%l) + "\n")
+        results[counter][1] = l
+        results[counter][2] = a
+        # f.write(str('%.6f'%l)+"\n")
+        counter += 1
+  
+
+
+def train_epoch(epoch, args, model, data_loader, optimizer):
+    model.train()
+    pid = os.getpid()
+    for batch_idx, (data, target) in enumerate(data_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        if not args.usemysgd:
+          loss.backward()
+          optimizer.step()
+        else:
+          optimizer.step(loss)
+        if batch_idx % args.log_interval == 0:
+            print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                pid, epoch, batch_idx * len(data), len(data_loader.dataset),
+                100. * batch_idx / len(data_loader), loss.item()))
+    
+
+
+def test_epoch(model, data_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in data_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1)[1] # get the index of the max log-probability
+            correct += pred.eq(target).sum().item()
+
+    test_loss /= len(data_loader.dataset)
+    accuracy = 100. * correct / len(data_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(data_loader.dataset),accuracy))
+    return (test_loss,accuracy)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
