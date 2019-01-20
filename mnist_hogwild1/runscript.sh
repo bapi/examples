149137fcabe3cac742f7adccd34d91b37f31309b
Bapi CHATTERJEE <bchatter@lndaal008.ist.local>
2019-01-20 16:33:12 +0100

    changes

--- mnist_hogwild1/runscript.sh
+++ mnist_hogwild1/runscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 8
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 18
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 28
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 38
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 48
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 58
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 68
-python3 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 78
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 8 2>&1 | tee log1
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 18 2>&1 | tee log2
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 28 2>&1 | tee log3
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 38 2>&1 | tee log4
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 48 2>&1 | tee log5
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 58 2>&1 | tee log6
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 68 2>&1 | tee log7
+~/anaconda3/bin/python3.7 main.py --lr 0.1 --epochs 100 --usemysgd 1 --test-batch-size 2000 --num-processes 78 2>&1 | tee log8
